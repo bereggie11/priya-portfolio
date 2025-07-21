c6038cca18678d9d30d2c564947acd17dffa91b4
regi <regi@apskc.net>
2025-07-21 00:45:11 -0500

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Routes from './Routes';
-import Page from './components/Page';
-import './index.css';
-import 'react-lazy-load-image-component/src/effects/blur.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import 'react-image-lightbox/style.css';
-import 'aos/dist/aos.css';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
+import Page from "./components/Page";
+import "./index.css";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "react-image-lightbox/style.css";
+import "aos/dist/aos.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = (): JSX.Element => {
   return (
@@ -15,6 +16,7 @@ const App = (): JSX.Element => {
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
+      <Analytics />
     </Page>
   );
 };
